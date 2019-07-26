90ef2491cf16f7ed65ad4998aca471747443637b
mackeynichols <mackey.nichols@gmail.com>
2019-07-25 22:56:45 -0400

    MKN: added unit details to response payload

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import requests, json
 class BungolScraper:
 
 	# set starting variables, specific to 150 sudbury and its environs
-	def __init__(self, address):
+	def __init__(self, address):	
 		
 		self.address = address.upper()
 
@@ -14,7 +14,7 @@ class BungolScraper:
 		self.req = requests.Session()
 		self.csrf_token = self.req.post("https://www.bungol.ca/map").headers['Set-Cookie'].split('=')[1].split(';')[0]
 
-		# headers we'll need to make requests
+		# assign headers we'll need to make requests
 		self.req_headers = {
 			"Host":"www.bungol.ca",
 			"Connection":"keep-alive",
@@ -37,31 +37,36 @@ class BungolScraper:
 
 		# build request URL for the input address
 		self.base_api_url = "https://www.bungol.ca/api/f/?min-lat="+self.address_coordinates["latitude"]+"&max-lat="+self.address_coordinates["latitude"]+"&min-lng="+self.address_coordinates["longitude"]+"&max-lng="+self.address_coordinates["longitude"]+"&sitl=730&low=0&high=0&show-active=true&show-sold=true&show-ended=false&transaction-type=Sale&list-view-activated=n&school-id=undefined&sb-activated=false&dom-range=x&screen-width=787&pt1=false&pt2=false&pt3=false&pt4=false&pt5=false&pt9=false&bed-0=false&bed-1=false&bed-1plus=false&bed-2=false&bed-3=false&bed-4=false&bed-5=false&bed-6=false&bath-1=false&bath-2=false&bath-3=false&bath-4=false&bath-5=false&bath-6=false&park0=false&park1=false&park2=false&park3=false&oh=false"
-		self.login_url = "/get-listing-data/c3940258-549696/"
 
   		# get CSRF token
 		self.req = requests.Session()
 		self.csrf_token = self.req.post("https://www.bungol.ca/map").headers['Set-Cookie'].split('=')[1].split(';')[0]
-
-		
-
 
 
 	def get_address_coordinates(self):
 
 		# get coordinates for input address
 		login_payload = {"csrfmiddlewaretoken" : self.csrf_token}
-		response =  json.loads(self.req.get( "https://www.bungol.ca/api/property-search-autocomplete/?value="+self.address.replace(" ", "%20"), data = json.dumps(login_payload), headers = self.req_headers ).text )
+		coordinates_response =  json.loads(self.req.get( "https://www.bungol.ca/api/property-search-autocomplete/?value="+self.address.replace(" ", "%20"), data = json.dumps(login_payload), headers = self.req_headers ).text )
 
-		return { "latitude" : response["addresses"][0]['la'], "longitude" : response["addresses"][0]['lo'] }
+		return { "latitude" : coordinates_response["addresses"][0]['la'], "longitude" : coordinates_response["addresses"][0]['lo'] }
 
 
 	def get_address_payload(self):
+		
 		# use the token to get a data payload for a specific address
+		response_payload = []
 		login_payload = {"csrfmiddlewaretoken" : self.csrf_token}	
 
-		response = self.req.get( self.base_api_url + self.login_url, data = json.dumps(login_payload), headers = self.req_headers ) 
-		response_payload = [ result for result in json.loads(response.text)["results"] if result['street'].upper() ==  self.address.upper() ]
+		# get basic pricing information for self.address
+		pricing_response = self.req.get( self.base_api_url, data = json.dumps(login_payload), headers = self.req_headers ) 
+		pricing_response_payload = [ result for result in json.loads(pricing_response.text)["results"] if result['street'].upper() ==  self.address.upper() ]
+
+		# get more detailed listing data (unit amenities, size details, etc)
+		for unit in pricing_response_payload:
+			address_listing_details_response = self.req.get( "https://www.bungol.ca/api/get-listing-data/"+unit['mls_number']+"-"+str(unit['id']), data = json.dumps(login_payload), headers = self.req_headers ) 
+			unit['listing_details'] = address_listing_details_response.text
+			response_payload.append( unit )
 
 		return response_payload
 
